8bfe021136821026d88b2deacd44076e58f71961
stefan-500 <wqygstefan900zwqi@gmail.com>
2025-02-18 10:51:00 +0100

    feat: ProductController * dodate rute i metode za prikaz osnovnih podataka o proizvodima i prikaz svih proizvoda specifične kategorije

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -3,9 +3,73 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use App\Models\Product;
 use Illuminate\Http\Request;
+use Throwable;
 
 class ProductController extends Controller
 {
-    //
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        // Prikaz podataka tabele products
+        $products = Product::get();
+
+        foreach ($products as $product) {
+            $product->regular_price = formatirajCijenu($product->regular_price);
+            $product->sale_price = formatirajCijenu($product->sale_price);
+        }
+
+        return response()->json($products);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Request $request, Product $product)
+    {
+        // Prikaz svih proizvoda određene kategorije
+        try {
+            $category = $request['category'];
+
+            if (!$category) {
+                return response()->json(["Loš zahtijev" => "Parametar je obavezan."], 400); // 400 => Bad Request
+            }
+
+            // Proizvod specificne kategorije, sa relacijama
+            $products = Product::whereHas('categories', function ($query) use ($category) {
+                $query->where('categories.id', $category);
+            })
+                ->with(['manufacturer', 'categories.departments'])
+                ->get();
+
+            foreach ($products as $product) {
+                $product->regular_price = formatirajCijenu($product->regular_price);
+                $product->sale_price = formatirajCijenu($product->sale_price);
+            }
+        } catch (Throwable $e) { // generalni Exception
+            return response()->json(["greška" => "Neuspiješno."], 500);
+        }
+
+        return response()->json($products); // uspijeh
+
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        // Brisanje proizvoda
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -2,10 +2,19 @@
 
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\DataController;
-use Illuminate\Http\Request;
+use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/products', [DataController::class, 'index']); // ispis podataka o proizvodima
+
+Route::get('/products', [DataController::class, 'index']); // ispis svih podataka vezanih za proizvod
+Route::get('/products-specific', [ProductController::class, 'index']); // ispis podataka svih proizvoda
+
+// Prikaz proizvoda specificne kategorije
+// Dodati request parametar: ?category=id
+Route::get('/category-products', [ProductController::class, 'show']);
 Route::get('/categories', [CategoryController::class, 'index']); // prikaz kategorija
-Route::put('/categories/{category}', [CategoryController::class, 'update']); // izmjena naziva kategorije
+
+// Izmijena naziva kategorije
+// Poslati PUT request na rutu: /api/categories/{category}, sa request body { "name": "Naziv kategorije" } (JSON)
+Route::put('/categories/{category}', [CategoryController::class, 'update']);
 Route::delete('/categories/{category}', [CategoryController::class, 'destroy']); // brisanje kategorije
